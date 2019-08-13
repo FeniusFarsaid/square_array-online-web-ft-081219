5a2157ab82bca1119cf8628a309533c0ca9363d0
David Ingram <david@ingramaviation.com>
2019-08-13 21:04:26 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -5,13 +5,3 @@ def square_array(array)
   # your code here
 end
 
-# def square_array(array)
-#   squared = []
-#   counter = 0 
-  
-#   while counter < array.length do
-#     squared << (array[counter] ** 2)
-#     counter += 1 
-#   end
-#   squared
-# end
